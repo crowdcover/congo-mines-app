9f5bcf77fc8fb3ec627fbae313aa8c6902aab5e3
Mark <mark.mcdonald@gmail.com>
2015-04-27 12:47:47 -0400

    new

--- db/migrate/20150427132625_modify_report_dates.rb
+++ db/migrate/20150427132625_modify_report_dates.rb
@@ -16,31 +16,40 @@ class ModifyReportDates < ActiveRecord::Migration
     }
 
     Report.all.each do |report|    
-      # nk  = k.gsub(/(\d+) (\w+) (\d{4})/, "#{$2}, #{$1}, #{$3}")
       replaced = report.date_string
       found = false
       
-      next if report.date_string.blank?
+      if report.date_string.present?
+        rept_parts = report.date_string.split("\s")
       
-      report.date_string.split("\s").each do |p|
-        puts "Beginning: #{p}"
-        if month_names.has_key?(p)
-          puts "found name: #{p}"
-          replaced = replaced.gsub(p, month_names[p])
-          puts replaced
-          found = true
-          break          
-        end 
-      end
-      
-      if found
-        nd =  Date.parse(replaced)
-        puts "NEW DATE: #{nd}"
-        report.actual_post_date = nd
+        if rept_parts.length == 1
+          report_actual_post_date = Date.parse("jan,1, #{rept_parts.first}") 
+        else
+          
+          rept_parts.each do |p|
+            puts "Beginning: #{p}"
+            if month_names.has_key?(p)
+              puts "found name: #{p}"
+              replaced = replaced.gsub(p, month_names[p])
+              puts replaced
+              found = true
+              break          
+            end 
+          end
+          
+          if found
+            nd =  Date.parse(replaced)
+            puts "NEW DATE: #{nd}"
+            report.actual_post_date = nd
+            report.save
+            puts "-" * 20      
+          end
+        end    
+      else
+        report.actual_post_date = report.post_date 
         report.save
-        puts "-" * 20      
-      end
+      end  
     end
   
-  end
+  end    
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150427132625) do
+ActiveRecord::Schema.define(version: 20150427124102) do
 
   create_table "attachments", force: :cascade do |t|
     t.string   "asset_file_name"
