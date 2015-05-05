f43d143f93cba6967ccae273af221c88c351635b
Charles Huang <charleshuang80@gmail.com>
2015-05-05 14:12:59 -0400

    edit get_geodata in drc_companies controller

--- app/controllers/drc_companies_controller.rb
+++ app/controllers/drc_companies_controller.rb
@@ -15,10 +15,11 @@ class DrcCompaniesController < ApplicationController
     # render json: @drc_companies
     render :index
   end
-  
+
   def get_geodata
     #get_geodata_drc_company_path(@drc_company)
-    @drc_company = DrcCompany.find(params[:id]).includes(:deposits, :social_projects, :processing_infrastructures)
+    @drc_company = DrcCompany.includes(:deposits, :social_projects,
+      :processing_infrastructures).find(params[:id])
   end
 
   private
@@ -28,6 +29,6 @@ class DrcCompaniesController < ApplicationController
 
   def drc_company_params
   end
-  
+
 
 end
