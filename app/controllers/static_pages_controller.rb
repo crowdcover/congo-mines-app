806cf7578eeed5ce4e1facc5beca6169e0de510e
Charles Huang <charleshuang80@gmail.com>
2015-05-06 18:55:10 -0400

    minor html tweaks to align with intended site design. added blank about page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,4 +2,7 @@ class StaticPagesController < ApplicationController
   def home
     @reports = Report.all.order('actual_post_date DESC') # need to add query for featured
   end
+
+  def about
+  end
 end
