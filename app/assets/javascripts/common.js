8ef057de2071cdc950a037a35b255f2a76fb4ff4
James Conkling <james.lane.conkling@gmail.com>
2015-05-05 20:10:16 -0400

    redesign sticky nav as foundation magellan component

--- app/assets/javascripts/common.js
+++ app/assets/javascripts/common.js
@@ -5,20 +5,9 @@ $(function(){
     initCommon: function(){
       mainbottom = $('.content').offset().top
 
-      $(window).on('scroll', this.showStickyHeader);
-
       $("#hideshow").click(function(){
         $("#advanced").slideToggle(500);
       });
-    },
-
-    showStickyHeader: function(){
-      stop = Math.round($(window).scrollTop());
-      if (stop > mainbottom) {
-          $('.menu').addClass('past-main');
-      } else {
-          $('.menu').removeClass('past-main');
-      }
     }
 
   };
