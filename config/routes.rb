482cc427becae1f036133850f956fa714179ed1f
Charles Huang <charleshuang80@gmail.com>
2015-05-07 01:28:47 -0400

    move js for all pages to application.html. added map controller and blank page.

--- config/routes.rb
+++ config/routes.rb
@@ -44,6 +44,7 @@ Rails.application.routes.draw do
   resources :reports, only: [:show, :index]
 
   resource :search
+  resource :map
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
