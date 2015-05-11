11d5dfe2213c1e9bf8249c19799cecf4937243eb
Mark <mark.mcdonald@gmail.com>
2015-05-11 15:12:22 -0400

    clean up controller

--- app/controllers/admin/reports_controller.rb
+++ app/controllers/admin/reports_controller.rb
@@ -1,28 +1,26 @@
 class Admin::ReportsController  < Admin::Auth # ApplicationController
   active_scaffold :"report" do |config|
+
+    config.columns = [:title,  :summary, :recommended, :source, :categories, 
+      :actual_post_date, :drc_companies] 
  
- 
-    config.list.per_page = 25 
+    config.list.per_page    = 25 
     config.create.multipart = 'local' 
     config.update.multipart = 'local' 
     
-    config.list.sorting = { :actual_post_date => :desc }   
-    config.columns = [:title, :recommended, :summary, :source, :actual_post_date, :categories, :drc_companies]
-    
-    #config.columns[:title].css_class = ' form-control input-lg'
- 
-    config.columns[:actual_post_date].label = 'Publication Date' 
+    config.list.sorting = { :actual_post_date => :desc }
+
+    config.columns[:actual_post_date].label   = 'Publication Date' 
     config.columns[:actual_post_date].form_ui = :date_picker
     config.columns[:actual_post_date].options = {options: [[changeYear: true]]}
     
-    #config.columns[:author].form_ui = :select
-    #config.columns[:source].form_ui = :select
-    
-    config.columns[:source].form_ui = :chosen
-    config.columns[:categories].form_ui = :chosen
-    config.columns[:drc_companies].form_ui = :chosen
+    config.columns[:source].form_ui         = :chosen
+    config.columns[:categories].form_ui     = :chosen
+    config.columns[:drc_companies].form_ui  = :chosen
     
     config.columns[:summary].form_ui = :text_editor
+
+    #config.columns[:title].css_class = ' form-control input-lg'
     
   end
 end
