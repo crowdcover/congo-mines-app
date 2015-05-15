080aa42406a6d0100a145a24a42d728b89ce00f2
Charles Huang <charleshuang80@gmail.com>
2015-05-14 23:55:11 -0400

    clean up drc company show page and map.js

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -28,61 +28,17 @@ $(function(){
 
         app.map.zoomControl.setPosition('topleft');
         L.control.scale().addTo(app.map);
-
-
-        // var mineIcon = L.icon({
-        //     // asset path
-        //     iconUrl: '/assets/mine.30.png',
-        //     iconSize: [30, 30],
-        //     popupAnchor: [0, -30]
-        // });
-        //
-        //
-        // var minesLayer;
-        //
-        // $.getJSON(/*ajax call url */, function(data) { // "/assets/data/mines.json"
-        //
-        //     minesLayer = L.geoJson(data, {
-        //       onEachFeature: onEachFeature
-        //     });
-        //
-        //     minesLayer.addTo(map);
-        //     app.setUpTable(data);
-        //   });
-
-
-        //For Each feature in the GeoJSON define popups, any action etc action
-        // function onEachFeature(feature, layer) {
-        //   var props = feature.properties;
-        //     var popupContent = "<h3 class='mine-marker'>"+ props.name + "</h3>"+
-        //         "Company: "+props.company + "<br />"+
-        //         "Type: " + props.type + "<br />"+
-        //         "Proven Reserves: "+ props.reserves.proven+ "<br />"+
-        //         "Source: " + props.source.author;
-        //
-        //     layer.bindPopup(popupContent);
-        // }
     },
 
     getGeodata: function(url){
 
-      // var mineIcon = L.icon({
-      //     // asset path
-      //     iconUrl: '/assets/mine.30.png',
-      //     iconSize: [30, 30],
-      //     popupAnchor: [0, -30]
-      // });
-
       var minesLayer;
 
       $.getJSON(url, function(data){
-        // console.log('inside getJSON');
 
-        // app.map.addjson(data)
         minesLayer = L.geoJson(data, {
           onEachFeature: onEachFeature
         });
-        // console.log(minesLayer);
 
         minesLayer.addTo(app.map);
         app.setUpTable(data);
@@ -105,14 +61,9 @@ $(function(){
 
 
     setUpTable: function(geoJSON){
-      //lets use Jquery DataTables
-      //in rails jquery-datatables-rails
       var table_data = [];
       var feature_array = $.makeArray( geoJSON.features )
       table_data = $.map(feature_array, function(d){ return d.properties;});
-
-      // console.log(feature_array);
-      // console.log('table_data', table_data);
 
       var titles = [];
       var tableDataArray = [];
@@ -129,7 +80,6 @@ $(function(){
         for (var prop in val) {
           oneDataRow.push(val[prop]);
         }
-        // console.log(oneDataRow);
         tableDataArray.push(oneDataRow);
       });
 
@@ -141,23 +91,6 @@ $(function(){
           "info": false,
           "searching": false,
           "columns": titles
-          // [
-          //   {"title": "name"},
-          //   {"title": "drc_company"},
-          //   {"title": "mine_type"},
-          //   {"title": "permit_type"},
-          //   {"title": "proven_reserves"},
-          //   {"title": "measured_resources"}
-          // ]
-
-          // [ titles ]
-          // [
-          //       { "data": "name" },
-          //       { "data": "drc_company" },
-          //       { "data": "mine_type" },
-          //       { "data": "proven_reserves" },
-          //       { "data": "measured_resources" }
-          //   ]
       });
 
     }
