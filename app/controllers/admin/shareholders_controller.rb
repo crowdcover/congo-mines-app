afdf8c65897b91cc1539430aaa4428229b1c2f3f
Mark <mark.mcdonald@gmail.com>
2015-05-19 12:31:03 -0400

    fixed shareholders

--- app/controllers/admin/shareholders_controller.rb
+++ app/controllers/admin/shareholders_controller.rb
@@ -4,6 +4,7 @@ class Admin::ShareholdersController  < Admin::Auth # ApplicationController
     config.label = 'Shareholder (en dernier ressort)'
 
     config.columns.exclude(:stock, :drc_companies, :created_at, :updated_at)
+   
 
     #config.columns[:drc_companies].options = {:draggable_lists => true}
     #config.columns[:drc_companies].form_ui = :chosen
