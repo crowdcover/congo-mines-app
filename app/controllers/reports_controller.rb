f8df88f37c17e17661358506b12a1b723dc41cfb
Charles Huang <charleshuang80@gmail.com>
2015-05-22 18:00:39 -0400

    added recommended documents in home page and library (documents index) page. Recommended Documents section set up to go away if no recommended documents (and currently there are no documents marked as recommended)

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -3,16 +3,15 @@ class ReportsController < ApplicationController
   def show
     @report = Report.includes(:attachments, :source, :author, :categories,
       :drc_companies).find(params[:id])
-    # render json: @report
     render :show
   end
 
   def index
-    # sort by most recent
-    @reports = Report.all.order('actual_post_date DESC').page params[:page]
-    # Report.featured
+    @reports = Report.all.order('actual_post_date DESC').page(params[:page]).per(12)
+    @rec_reports = Report.where(recommended: true).order('actual_post_date DESC').page(params[:page]).per(2)
 
-    # render json: @reports
+    #for testing while no reports marked as recommended
+    # @rec_reports = Report.all.order('actual_post_date DESC').page(params[:page]).per(2)
     render :index
   end
 
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,10 @@
 class StaticPagesController < ApplicationController
   def home
-    @reports = Report.all.order('actual_post_date DESC') # need to add query for featured
+    @rec_reports = Report.where(recommended: true).order('actual_post_date DESC').take(4)
+
+    # use this variable on home page (instead of @rec_reports) and use that partial
+    # while there are no reports marked as recommended
+    @reports = Report.all.order('actual_post_date DESC').take(4)
   end
 
   def about
