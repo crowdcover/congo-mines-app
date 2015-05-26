819c33cf0c94cd109be66189c70bdd69d14575b3
Mark <mark.mcdonald@gmail.com>
2015-05-26 10:57:48 -0400

    new

--- db/migrate/20150526144027_change_shareholders.rb
+++ db/migrate/20150526144027_change_shareholders.rb
@@ -1,7 +1,7 @@
 class ChangeShareholders < ActiveRecord::Migration
   def change
       change_table :shareholders do |t|
-      t.remove :community_fund, :community_dialogue_platform
+      t.remove  :community_dialogue_platform
   
       t.string :community_fund
       t.string :community_dialogue_platform
