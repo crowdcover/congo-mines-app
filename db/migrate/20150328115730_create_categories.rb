e8ae6845b9e7e4196962e29d4f22b392082ccaff
Mark <mark.mcdonald@gmail.com>
2015-06-02 11:05:03 -0400

    removed the creation of categories because this file is loaded when the the miration runs for a conversion of psql to sqlite

--- db/migrate/20150328115730_create_categories.rb
+++ db/migrate/20150328115730_create_categories.rb
@@ -1,6 +1,6 @@
 class CreateCategories < ActiveRecord::Migration
   def change
-  
+
     create_table :categories do |t|
       t.integer :position
       t.string :name
@@ -9,23 +9,24 @@ class CreateCategories < ActiveRecord::Migration
 
       t.timestamps null: false
     end
-    
+
+=begin
     current_category = nil
     current_second_tier = nil
     File.open("#{Rails.root}/db/Categories 26.3.txt").each_line do |l|
-      
+
       if l !~ /^\s+/
         current_category = Category.create(name: l.strip)
-        puts current_category        
+        puts current_category
       else
         parts = l.split(/\t/)
         if parts[1].present?
-          current_second_tier = current_category.children.create(name: l.strip)  
+          current_second_tier = current_category.children.create(name: l.strip)
         else
           current_second_tier.children.create(name: l.strip)
-        end    
+        end
       end
     end
-  
+=end
   end
 end
