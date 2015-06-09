06b8dae20a97cf8b0231b3b850e7f8057fdca214
Mark <mark.mcdonald@gmail.com>
2015-06-09 16:04:20 -0400

    getting search stuff to work correctly....

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -7,33 +7,33 @@ class SearchesController < ApplicationController
     #@search = Report.
     #s = DrcCompany.search{|q| q.fulltext ' rapport de l’association' }
 
-    search = Report.search do |q|
-       q.fulltext params[:search]
-       q.paginate page: params[:page], per_page: 12
+    categories = []
+    categories = [:theme, :type_document, :type_source, :province].map{|c| params[c].to_i }.compact
+
+    categories.delete(0)
+
+    search = Report.search do
+       fulltext params[:search]
+       #q.keywords  params[:search]
+       with(:category_ids).all_of(categories) unless categories.blank?
+       order_by :actual_post_date, :desc
+       paginate page: params[:page], per_page: 12
     end
-    @reports = @results = search.results
 
     #binding.pry
-=begin
-    # Good For DRC Company
-    search = DrcCompany.search do |q|
-       q.fulltext params[:search]
-       q.paginate page: params[:page], per_page: 12
-    end
-    @results = search.results
-    @reports = @results.map(&:reports).first
-=end
+    @reports = @results = search.results
 
     render :show2
   end
 
   protected
   def search(options)
-    categories = []
 
-    categories << options[:theme].to_i << options[:type_document].to_i <<
-      options[:type_source].to_i << options[:province].to_i
-    categories.delete(0) # removing blank categories
+    categories = [:theme, :type_document, :type_source, :province].map{|c| options[c] }.reject(&:empty?)
+
+    #categories << options[:theme].to_i << options[:type_document].to_i <<
+    #  options[:type_source].to_i << options[:province].to_i
+    #categories.delete(0) # removing blank categories
     # fail
     Sunspot.search(Report) do
       with(:category_ids).all_of(categories)
