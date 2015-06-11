f6722d9ed2986ddc8f51a44be9c7c8b25db396d1
Mark <mark.mcdonald@gmail.com>
2015-06-11 09:00:06 -0400

    added bootstrap to admin css added validations for lat/lng

--- app/models/deposit.rb
+++ app/models/deposit.rb
@@ -2,6 +2,7 @@ class Deposit < ActiveRecord::Base
   belongs_to :drc_company
   #belongs_to :deposit_source
 
+
   has_many :proven_reserves, -> { where deposit_type:  DepositResource::PROVEN_RESERVES },
    as: :depoable, dependent: :destroy, class_name: 'DepositResource'
 
@@ -23,5 +24,13 @@ class Deposit < ActiveRecord::Base
   has_many :total_resources, -> { where deposit_type: DepositResource::TOTAL_RESOURCES },
    as: :depoable, dependent: :destroy, class_name: 'DepositResource'
 
-  validates_presence_of :name, :lat, :lng
+  #validates_presence_of :name, :lat, :lng
+
+  validates :lat, numericality: { greater_than_or_equal_to: -90,
+                                  less_than_or_equal_to: 90 },
+                  presence: true
+
+  validates :lng, numericality: { greater_than_or_equal_to: -180,
+                                  less_than_or_equal_to: 180 },
+                  presence: true
 end
