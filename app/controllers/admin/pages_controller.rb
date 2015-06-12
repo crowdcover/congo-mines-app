2ba46620bb227b6d913b138cf11770c8595b1d15
Mark <mark.mcdonald@gmail.com>
2015-06-12 11:33:00 -0400

    remove banner and title from pages for now

--- app/controllers/admin/pages_controller.rb
+++ app/controllers/admin/pages_controller.rb
@@ -1,22 +1,16 @@
 class Admin::PagesController < Admin::Auth
 
     # not listing/editing topic/ :category,
-  columns =  [:topic, :title,  :content,  :banner]
-  create_columns = columns - [:topic]
+  columns =  [:topic,  :content]  # :banner (image)
+  #create_columns = columns - [:topic]
 
 
 
   active_scaffold :"page" do |config|
     config.actions = [:list, :update, :show]
-    config.list.columns = columns
-    config.update.columns = create_columns
-    config.update.columns = create_columns
+    config.list.columns = [:topic]
 
-    config.columns[:category].form_ui = :chosen
-
-    config.columns[:topic].form_ui = :select
-    config.columns[:topic].options = {include_blank: 'Select Topic',
-      options: ['EITI', 'Mining Code', 'Sicomines', 'State Enterprises', 'Human Rights'] }
+    config.update.columns = columns
 
     config.columns[:content].form_ui = :text_editor
 
@@ -39,23 +33,6 @@ class Admin::PagesController < Admin::Auth
         #plugins: "preview",
       }
     }
-
-=begin
-    config.columns[:content].options = {
-      tinymce: {
-        	selector: "textarea",
-	        plugins: ["advlist autolink lists link image charmap print preview anchor",
-		"searchreplace visualblocks code fullscreen",
-		"insertdatetime media table contextmenu paste moxiemanager"],
-
-	      toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify |      bullist numlist outdent indent | link image",
-	      autosave_ask_before_unload: false
-
-        # :theme => 'advanced',
-        # :editor_css => '/product_editor.css'
-      }
-    }
-=end
 
   end
 end
