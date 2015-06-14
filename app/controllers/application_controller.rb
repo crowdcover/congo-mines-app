f02ac25ffe5677a8bec6e3317f601ca7c9e501cb
Mark <mark.mcdonald@gmail.com>
2015-06-14 06:53:44 -0400

    added a image_url for front page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,21 +4,22 @@ class ApplicationController < ActionController::Base
 
   before_filter :set_banner_image
   protect_from_forgery with: :exception
-  
+
   #http_basic_authenticate_with name: "congo", password: "mines" #, except: :index
-  
+
   layout 'application'
-  
+
   before_action :set_locale
-   
+
   def set_locale
     I18n.locale =  params[:locale] || I18n.default_locale
   end
 
   # set banner image
   def set_banner_image
-    @banner_url = '/assets/header_banner_narrow.jpg'
+    #@banner_url = '/assets/header_banner_narrow.jpg'
+    @banner_url = 'header_banner_narrow.jpg'
     @pages = Page.all.order(:position)
   end
-  
+
 end
