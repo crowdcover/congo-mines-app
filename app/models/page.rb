735d1ba762fb15c9dcd16a8f3ad0eee06ca07d1b
Mark <mark.mcdonald@gmail.com>
2015-06-15 11:17:48 -0400

    try this with images...

--- app/models/page.rb
+++ app/models/page.rb
@@ -11,14 +11,14 @@ class Page < ActiveRecord::Base
 
   validates_uniqueness_of :topic, if: :topic_changed?
 
-
+  DEFAULT_URL = "#{Rails.configuration.action_controller.asset_host}#{Rails.configuration.assets.prefix}/default_topic_img.jpg"
   has_attached_file :banner,
     styles: { :medium => "300x300>", :thumb => "100x100>" },
-
-    default_url: ActionController::Base.helpers.asset_path('default_topic_img.jpg')
+    default_url: DEFAULT_URL
+    #default_url: ActionController::Base.helpers.asset_path('default_topic_img.jpg')
     #:default_url => "/assets/default_topic_img.jpg"
     # default_url: ->(attachment) { 'default_topic_img.jpg' }
-    
+
   validates_attachment_content_type :banner, :content_type => /\Aimage\/.*\Z/
 
   def label
