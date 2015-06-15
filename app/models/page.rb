0b5773ebb3c97d4ab6deea21d9ce510a75973d95
Mark <mark.mcdonald@gmail.com>
2015-06-15 12:24:32 -0400

    changes a relationship

--- app/models/page.rb
+++ app/models/page.rb
@@ -13,8 +13,8 @@ class Page < ActiveRecord::Base
 
   DEFAULT_URL = "#{Rails.configuration.action_controller.asset_host}#{Rails.configuration.assets.prefix}/default_topic_img.jpg"
   has_attached_file :banner,
-    styles: { :medium => "300x300>", :thumb => "100x100>" },
-    default_url: DEFAULT_URL
+    styles: { :medium => "300x300>", :thumb => "100x100>" }
+    #default_url: DEFAULT_URL
     #default_url: ActionController::Base.helpers.asset_path('default_topic_img.jpg')
     #:default_url => "/assets/default_topic_img.jpg"
     # default_url: ->(attachment) { 'default_topic_img.jpg' }
