331f3869046293519275b7c38b8ac41e7d00e10c
Mark <mark.mcdonald@gmail.com>
2015-06-19 06:14:55 -0400

    modified the pg backup date to be 20 days before file are deleted.

--- scripts/cron/db_dump_house_keeping.sh
+++ scripts/cron/db_dump_house_keeping.sh
@@ -3,11 +3,10 @@ path=/var/pgdump
 logfile=/var/log/$0
 
 rm -f $logfile
-for file in `find /home/mark/backup/pg_dump/ -mtime +30 -type f -name *.sql.gz`
+for file in `find /home/mark/backup/pg_dump/ -mtime +20 -type f -name '*.sql*'`
 do
   echo "deleting: " $file >> $logfile
   rm $file
 done
 
 exit 0
-
