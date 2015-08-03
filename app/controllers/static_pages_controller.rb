b1638a135c1db952cce68b8dbfc069d793b353f6
Kristofor Carle <kristoforcarle@moabi.org>
2015-08-03 17:50:34 -0400

    load 4 recommended reports instead of 3

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,6 @@
 class StaticPagesController < ApplicationController
   def home
-    @rec_reports = Report.where(recommended: true).order('actual_post_date DESC').take(3)
+    @rec_reports = Report.where(recommended: true).order('actual_post_date DESC').take(4)
 
     # use this variable on home page (instead of @rec_reports) and use that partial
     # while there are no reports marked as recommended
