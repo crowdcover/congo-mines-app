d47de3c4d07274df6476786f4b6b84778a6583b9
Kristofor Carle <kris@maphubs.com>
2017-04-13 22:42:12 -0400

    add missing auth example, config with env vars so it can be checked in…

---
+++ app/controllers/admin/auth.rb
@@ -0,0 +1,14 @@
+class Admin::Auth  < ActionController::Base # < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+  http_basic_authenticate_with name: ENV['ADMIN_USER'], password: ENV['ADMIN_PASS'] #, except: :index
+
+  layout 'admin/auth'
+
+  before_action :set_locale
+  def set_locale
+    I18n.locale = 'fr' # params[:locale] || I18n.default_locale  #:fr
+  end
+
+end
