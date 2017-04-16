69217200a3af330fcbc99cfd95bdc45db9e9296c
Kristofor Carle <kris@maphubs.com>
2017-04-16 18:54:18 -0400

    add meaningful page titles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,8 +3,20 @@ module ApplicationHelper
     current_page?(link_path) ? "active" : ""
   end
 
-   def current_locale?(locale)
+  def current_locale?(locale)
     I18n.locale == locale
   end
+
+  def title
+    if content_for?(:title)
+      # allows the title to be set in the view by using t(".title")
+      content_for :title
+    else
+      # look up translation key based on controller path, action name and .title
+      # this works identical to the built-in lazy lookup
+      t("#{ controller_path.tr('/', '.') }.#{ action_name }.title", default: :site_name)
+    end
+  end
+
 
 end
