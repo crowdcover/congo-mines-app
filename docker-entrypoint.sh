44720b8e333a08b0414f54bd2a7f748f1e353e6a
Kristofor Carle <kris@maphubs.com>
2017-04-17 18:43:57 -0400

    attempt to fix solr docker volume issues

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
-rake sunspot:solr:start
+#repair solr volume
+chown -R solr:solr /app/solr
+rm -rf /app/solr/pids
+#start solr
+bundle exec rake sunspot:solr:start
+#start server
 bundle exec rails s -p 3000 -b '0.0.0.0'
