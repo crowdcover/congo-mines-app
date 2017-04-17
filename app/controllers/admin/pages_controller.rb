59b41b9342bb805613d826677544fa18b14d028b
Kristofor Carle <kris@maphubs.com>
2017-04-17 16:36:41 -0400

    remove sortable config

--- app/controllers/admin/pages_controller.rb
+++ app/controllers/admin/pages_controller.rb
@@ -15,8 +15,8 @@ class Admin::PagesController < Admin::Auth
     config.columns[:content].form_ui = :text_editor
 
 
-    config.actions << :sortable
-    config.sortable.column = :position
+    #config.actions << :sortable
+    #config.sortable.column = :position
 
     config.columns[:content].options = {
       :tinymce => {
