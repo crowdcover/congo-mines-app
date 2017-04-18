20d795b9f1de2ff095c07be78c38dd01c02eac5e
Kristofor Carle <kris@maphubs.com>
2017-04-17 20:01:55 -0400

    attempt to fix startup issues

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
-#repair solr volume
-chown -R solr:solr /app/solr
-rm -rf /app/solr/pids
 #start solr
+bundle exec rake assets:precompile
 bundle exec rake sunspot:solr:start
+bundle exec rake sunspot:solr:reindex
 #start server
 bundle exec rails s -p 3000 -b '0.0.0.0'
