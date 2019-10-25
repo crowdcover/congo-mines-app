bb5b186b430cb86412a5c2be9fc0382edbd78efd
Kristofor Carle <kris@maphubs.com>
2019-10-24 23:11:32 -0400

    fix Dotenv causes error in production

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-Dotenv::Railtie.load
+Dotenv::Railtie.load if %w[development test].include? ENV['RAILS_ENV']
 
 module ActiveScaffold
   class Application < Rails::Application
